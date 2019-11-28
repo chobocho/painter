34a081a188aebfc51c2b9c9de62210762855ba51
seunghwa.cho <chobocho99@gmail.com>
2019-11-29 00:21:44 +0900

    Add draw line

--- doc/tutorial/004/src/painter.js
+++ doc/tutorial/004/src/painter.js
@@ -1,5 +1,9 @@
 var cvs;
 var canvas;
+
+var bufCanvas;
+var bufCtx;
+
 var painter;
 
 function Point(x_, y_) {
@@ -9,6 +13,12 @@ function Point(x_, y_) {
 
 Point.prototype.toString = function () {
     return '(' + this.x + "," + this.y + ")";
+}
+
+function getMousePosition(event) {
+  var x = event.pageX - canvas.offsetLeft;
+  var y = event.pageY - canvas.offsetTop;
+  return { X: x, Y: y };
 }
 
 function MouseAction(mouseUp, mouseDown, mouseMove) {
@@ -21,7 +31,7 @@ function Shape(name_) {
     this.name = name_;
     this.point = new Point(0, 0);
     this.color = 0;
-    this.state = 0;
+    this.state = false;
     this.filled = false;
     this.mouseAction;
 }
@@ -36,8 +46,67 @@ Shape.prototype.toString = function() {
            ')';
 }
 
+Shape.prototype.isDraw = function () {
+    return this.state;
+}
+
+Shape.prototype.setDrawMode = function (state) {
+    return this.state = state;
+}
+
+
+function lineMouseUp(event) {
+  if (!painter.isDraw()) {
+    return;
+  }
+  console.log("lineMouseUp");
+  var currentPos = getMousePosition(event);
+  bufCtx.beginPath();
+  bufCtx.strokeStyle = painter.shape.color;
+  bufCtx.moveTo(painter.shape.point.x, painter.shape.point.y);
+  bufCtx.lineTo(currentPos.X, currentPos.Y);
+  bufCtx.closePath();
+  bufCtx.stroke();
+  cvs.drawImage(bufCanvas, 0, 0);
+
+  painter.setDrawMode(false);
+}
+
+function lineMouseDown(event) {
+  console.log("lineMouseDown");
+  if (painter.isDraw()) {
+    return;
+  }
+  bufCtx.drawImage(canvas, 0, 0);
+  var startPos = getMousePosition(event);
+  painter.shape.point.x = startPos.X;
+  painter.shape.point.y = startPos.Y;
+  painter.setDrawMode(true);
+}
+
+function lineMouseMove(event) {
+  if (!painter.isDraw()) {
+    return;
+  }
+  console.log("lineMouseMove");
+  
+  var currentPos = getMousePosition(event);
+  cvs.beginPath();
+  // Need a delay
+  cvs.clearRect(0, 0, canvas.width, canvas.height);
+  cvs.drawImage(bufCanvas, 0, 0);
+
+  cvs.strokeStyle = painter.shape.color;
+  cvs.moveTo(painter.shape.point.x, painter.shape.point.y);
+  cvs.lineTo(currentPos.X, currentPos.Y);
+  cvs.closePath();
+  cvs.stroke();
+}
+
+
 function Painter() {
-    this.shape = new Shape("Point");
+    this.shape = new Shape("Line");
+    this.shape.mouseAction = new MouseAction(lineMouseUp, lineMouseDown, lineMouseMove);
 }
 
 Painter.prototype.toString = function () {
@@ -46,18 +115,30 @@ Painter.prototype.toString = function () {
            ')';
 }
 
+Painter.prototype.isDraw = function () {
+    return this.shape.isDraw();
+}
+
+Painter.prototype.setDrawMode = function (state) {
+    return this.shape.setDrawMode(state);
+}
+
 function mouseListener(event) {
   switch (event.type) {
     case "mousedown":
       console.log("mousedown");
+      painter.shape.mouseAction.down(event);
       break;
     case "mousemove":
       console.log("mousemove");
+      painter.shape.mouseAction.move(event);
       break;
     case "mouseup":
       console.log("mouseup");
+      painter.shape.mouseAction.up(event);
       break;
     case "mouseout":
+      painter.shape.mouseAction.up(event);
       console.log("mouseout");
       break;
   }
@@ -66,6 +147,11 @@ function mouseListener(event) {
 function onLoadPage() {
   canvas = document.getElementById("canvas");
   cvs = canvas.getContext("2d");
+
+  bufCanvas = document.createElement("canvas");
+  bufCanvas.width = canvas.width;
+  bufCanvas.height = canvas.height;
+  bufCtx = bufCanvas.getContext("2d");
 
   canvas.addEventListener("mousedown", mouseListener);
   canvas.addEventListener("mousemove", mouseListener);
